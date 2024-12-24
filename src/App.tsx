55f85a72b03d071bfdfeb696bbaea441f9cd4497
Sadykov Arsen <LoCo10291998@gmail.com>
2024-12-24 18:33:36 +0600

    Level 3 is done. Created button "Get Cards" in App.tsx. And made showing copy of a deck of cards.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import './App.css'
+import CardDeck from "./lib/CardDeck.ts";
+import {useState} from "react";
 import CardItem from "./Components/CardItem/CardItem.tsx";
+import Card from "./lib/Card.ts";
+
+const cardDeck = new CardDeck();
+
 
 const App = () => {
+    const [hand, setHand] = useState<Card[]>([]);
 
-  return (
-      <>
-        <div className="playingCards faceImages">
-          <CardItem rank="K" suit="diams"/>
-        </div>
-      </>
-  )
+    const getFiveCards = () => {
+        setHand(cardDeck.getCards(5));
+    };
+
+    return (
+        <>
+            <h4>Cards: {cardDeck.deck.length}</h4>
+            <hr/>
+            {cardDeck.deck.length < 5 ?
+                null
+                :
+                <button onClick={getFiveCards}>Get Cards</button>}
+            <hr/>
+            <div className="playingCards faceImages">
+                {hand.map((card) => (
+                    <CardItem
+                        key={card.rank + card.suit}
+                        rank={card.rank}
+                        suit={card.suit}
+                    />
+                ))}
+            </div>
+
+        </>
+    )
 };
 
 export default App
