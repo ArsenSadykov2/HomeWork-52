1cfae17f5401c72af41346bffff8c16744361192
Sadykov Arsen <LoCo10291998@gmail.com>
2024-12-24 18:53:28 +0600

    Level 4 is done. Created PokerHand.ts. And Created the ability to determine the best hand in a deck of cards.

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,44 @@ import CardDeck from "./lib/CardDeck.ts";
 import {useState} from "react";
 import CardItem from "./Components/CardItem/CardItem.tsx";
 import Card from "./lib/Card.ts";
+import PokerHand from "./lib/PokerHand.ts";
 
 const cardDeck = new CardDeck();
 
 
 const App = () => {
     const [hand, setHand] = useState<Card[]>([]);
+    const [combination, setCombination] = useState<string>('');
 
     const getFiveCards = () => {
-        setHand(cardDeck.getCards(5));
+        let fiveCards = cardDeck.getCards(5)
+        setHand(fiveCards);
+
+        const pokerHand = new PokerHand(fiveCards);
+        setCombination(pokerHand.getOutcome());
     };
 
     return (
         <>
-            <h4>Cards: {cardDeck.deck.length}</h4>
-            <hr/>
-            {cardDeck.deck.length < 5 ?
-                null
-                :
-                <button onClick={getFiveCards}>Get Cards</button>}
-            <hr/>
-            <div className="playingCards faceImages">
-                {hand.map((card) => (
-                    <CardItem
-                        key={card.rank + card.suit}
-                        rank={card.rank}
-                        suit={card.suit}
-                    />
-                ))}
+            <div className="container">
+                <h4>Cards: {cardDeck.deck.length}</h4>
+                <hr/>
+                <p>Cards of round: {combination}</p>
+                <hr/>
+                {cardDeck.deck.length < 5 ?
+                    null
+                    :
+                    <button onClick={getFiveCards}>Get Cards</button>}
+                <hr/>
+                <div className="playingCards faceImages">
+                    {hand.map((card) => (
+                        <CardItem
+                            key={card.rank + card.suit}
+                            rank={card.rank}
+                            suit={card.suit}
+                        />
+                    ))}
+                </div>
             </div>
 
         </>
